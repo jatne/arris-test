8b93af1928cc3c847bb607ad83d87241adf67db0
Paweł Malok <pawelmalok@gmail.com>
2020-05-20 15:18:28 +0200

    init 🚀

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-uglifycss');
+const minifyJS = require('gulp-uglify');
+
+const onError = err => console.log(err);
+
+const paths = {
+  scss: {
+    src: './src/scss/style.scss',
+    dest: './dist/assets/css/',
+  },
+  js: {
+    src: './src/js/app.js',
+    dest: './dist/assets/js/',
+  },
+};
+
+const style = () =>
+  gulp
+    .src(paths.scss.src)
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(sass().on('error', onError))
+    .pipe(autoprefixer())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(paths.scss.dest));
+
+
+const js = () =>
+  gulp
+    .src(paths.js.src)
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(babel())
+    .pipe(minifyJS())
+    .pipe(gulp.dest(paths.js.dest));
+
+
+
+const watcher = () => {
+  gulp.watch('./src/scss/**/*.scss', style);
+  gulp.watch(paths.js.src, js);
+};
+
+const styleWatcher = gulp.series(
+  style,
+  js,
+  watcher
+);
+
+exports.default = styleWatcher;
